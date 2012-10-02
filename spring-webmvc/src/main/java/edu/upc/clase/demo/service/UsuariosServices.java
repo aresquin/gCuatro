b5afea5608bf977c27a0dcf42382527649bd1fbf
Johan Cholan <spike14_11@hotmail.com>
2012-10-01 21:15:55 -0500

    cambios usuarios
    cambios usuarios

--- spring-webmvc/src/main/java/edu/upc/clase/demo/service/UsuariosServices.java
+++ spring-webmvc/src/main/java/edu/upc/clase/demo/service/UsuariosServices.java
@@ -5,13 +5,4 @@ import java.util.List;
 
 public interface UsuariosServices 
 {
-    public Integer insertar(Usuarios usuarios);
-    public void actualizar(Usuarios usuarios);
-    public void eliminar(Usuarios usuarios);
-    public List<Usuarios> buscarTodos();
-    public Usuarios buscarXcodigo(Integer id);
-    public List<Usuarios> buscarPorNombre(String nombre);
-    public Boolean autenticar(String correo,String password);  
-    public Boolean validarCorreo(String correo);
-    public Boolean validarContrasena(String contrasena);
 }
